0c464e18ac0fbc6b0a8206d44aab1c15c502d6c8
Daniela Farias <farias.danicris@gmail.com>
2021-11-15 15:12:26 -0300

    pegando o response da api da deezer

--- index.js
+++ index.js
@@ -1,10 +1,11 @@
+require("dotenv").config();
 const express = require("express");
 const path = require("path");
+const Song = require("./models/songs");
+const axios = require("axios");
+
 const app = express();
 const port = process.env.PORT || 3000;
-const Song = require("./models/songs");
-
-require('dotenv').config();
 
 app.set("view engine", "ejs");
 
@@ -12,57 +13,72 @@ app.use(express.urlencoded());
 app.use(express.static(path.join(__dirname, "public")));
 app.use(express.json());
 
-// const songs = [
-//     {
-//         id: 1,
-//         cover: "https://upload.wikimedia.org/wikipedia/pt/0/00/Sawayama.jpg",
-//         title: "XS",
-//         artist: "Rina Sawayama",
-//         album: "Sawayama"
-//     },
-// ];
-
 var message = "";
 
 /* Renderiza a página inicial */
 app.get("/", async (req, res) => {
-    const songs = await Song.findAll();
+  const songs = await Song.findAll();
 
-    res.render("index", { pageTitle: "JOYMUSIC | Home", songs, message });
+  res.render("index", { pageTitle: "JOYMUSIC | Home", songs, message });
 });
 
 /* Renderiza a página de cadastrar música */
 app.get("/cadastro", (req, res) => {
-    res.render("cadastro", { pageTitle: "Joymusic | Cadastro de Música" });
+  res.render("cadastro", { pageTitle: "Joymusic | Cadastro de Música" });
 });
 
+/* Renderiza a página de detalhes */
 app.get("/detalhes/:id", async (req, res) => {
-    const songs = await Song.findByPk(req.params.id);
-    res.render("detalhes", { pageTitle: "Joymusic | Informações da Música", songs });
-  });
+  const songs = await Song.findByPk(req.params.id);
+  const songTitle = songs.title;
+  const songArtist = songs.artist;
+  const songAlbum = songs.album;
 
-  app.post("/new", async (req, res) => {
-    const { cover, title, artist, album } = req.body;
+  const params = {
+    method: "GET",
+    url: "https://api.deezer.com/search?q=" + songTitle,
+    headers: {
+      "Content-Type": "application/json",
+    },
+  };
 
-    try {
-      await Song.create({
-        cover, 
-        title, 
-        artist, 
-        album
-      });
+  const response = await axios(params);
+
+  const data = response.data;
+
   
+
+  console.log(data);
+
+  res.render("detalhes", {
+    pageTitle: "Joymusic | Informações da Música",
+    songs,
+  });
+});
+
+/* Rota para Post de uma nova música */
+app.post("/new", async (req, res) => {
+  const { cover, title, artist, album } = req.body;
+
+  try {
+    await Song.create({
+      cover,
+      title,
+      artist,
+      album,
+    });
+
     message = "A música foi cadastrada com sucesso!";
     res.redirect("/");
-    } catch (err) {
-      console.log(err);
-      res.render("cadastro", {
-        pageTitle: "Joymusic | Cadastro de Música",
-      });
-    }
-  });
+  } catch (err) {
+    console.log(err);
+    res.render("cadastro", {
+      pageTitle: "Joymusic | Cadastro de Música",
+    });
+  }
+});
 
-/* Rota para alterar apenas uma música */
+/* Renderizar página de alteração */
 app.get("/update/:id", async (req, res) => {
   const songs = await Song.findByPk(req.params.id);
 
@@ -71,10 +87,11 @@ app.get("/update/:id", async (req, res) => {
       message: "Música não encontrado!",
     });
   }
-  
+
   res.render("update", { pageTitle: "Joymusic | Edite a Música", songs });
 });
 
+/* Rota para alterar a música  */
 app.post("/update/:id", async (req, res) => {
   const songs = await Song.findByPk(req.params.id);
 
@@ -86,11 +103,15 @@ app.post("/update/:id", async (req, res) => {
   songs.album = album;
 
   const updatedSong = await songs.save();
-  
-  res.render("update", { pageTitle: "Joymusic | Editar Música", songs: updatedSong, message: "Sucesso ao editar a música!" });
+
+  res.render("update", {
+    pageTitle: "Joymusic | Editar Música",
+    songs: updatedSong,
+    message: "Sucesso ao editar a música!",
+  });
 });
 
-/* Rotas para deletar */
+/* Rotas para renderizar a página de confirmação de delete */
 app.get("/deletar/:id", async (req, res) => {
   const songs = await Song.findByPk(req.params.id);
   if (!songs) {
@@ -105,6 +126,7 @@ app.get("/deletar/:id", async (req, res) => {
   });
 });
 
+/* Rota para deletar a música */
 app.post("/deletar/:id", async (req, res) => {
   const songs = await Song.findByPk(req.params.id);
   if (!songs) {
@@ -118,5 +140,6 @@ app.post("/deletar/:id", async (req, res) => {
   res.redirect("/");
 });
 
-app.listen(port, () => console.log(`Servidor rodando em http://localhost:${port}`));
-
+app.listen(port, () =>
+  console.log(`Servidor rodando em http://localhost:${port}`)
+);
