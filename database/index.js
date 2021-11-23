5d3b6ac764399d04de58308ee3d319d5e7acb501
Daniela Farias <farias.danicris@gmail.com>
2021-11-22 21:01:43 -0300

    Adição do DATABASE_URL no env

--- database/index.js
+++ database/index.js
@@ -1,13 +1,11 @@
 const Sequelize = require("sequelize");
+require('dotenv').config()
 
 // const sequelize = new Sequelize(process.env.DB_BASE, process.env.DB_USER,process.env.DB_PASS, {
 //   host:  process.env.DB_HOST,
 //   port: process.env.DB_PORT,
 //   dialect: 'postgres'
 // });
-
-
-// Conectando com o DB do Heroku
 
 const sequelize = new Sequelize(process.env.DATABASE_URL, {     
   dialect: 'postgres',
